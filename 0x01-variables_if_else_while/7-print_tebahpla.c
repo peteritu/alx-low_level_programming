d843d59068ee132bf69c9741bc6a63c9ca658bdd
peteritu <petendiritu@gmail.com>
2023-10-09 14:20:40 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    char letter = 'z';
+
+    while (letter >= 'a')
+    {
+        putchar(letter);
+        letter--;
+    }
+
+    putchar('\n');
+    return (0);
+}
