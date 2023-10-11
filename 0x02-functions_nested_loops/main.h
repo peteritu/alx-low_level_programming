73fce7fc4a8871c8252c542fdcc8d004ef402f81
peteritu <petendiritu@gmail.com>
2023-10-11 13:54:38 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,13 @@
 int _putchar(char c);
 
 #endif /* MAIN_H */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+void print_alphabet(void);
+void print_alphabet_x10(void); // Function prototype for print_alphabet_x10
+
+#endif /* MAIN_H */
