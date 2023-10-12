f4c21d5f7f512c74af2f168b9d2f81fc8f1debed
peteritu <petendiritu@gmail.com>
2023-10-12 15:29:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,14 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-* main - Entry point of the program
+* main - Entry point
 *
-* Description: This program prints "_putchar" followed by a new line and
-* returns 0 to indicate successful execution.
+* Description: Prints "_putchar" followed by a new line
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
+printf("_putchar\n");
 return (0);
 }
