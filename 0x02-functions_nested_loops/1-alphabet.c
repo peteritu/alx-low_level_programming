d2cf659451b84fe882eb0a094ed6257723f22072
peteritu <petendiritu@gmail.com>
2023-10-12 15:47:22 -0700

    Committing changes to 2-print_alphabet.c and 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - Prints the alphabet in lowercase followed by a new line.
+* print_alphabet - Prints the lowercase alphabet followed by a new line
 */
 void print_alphabet(void)
 {
@@ -12,6 +12,5 @@ while (letter <= 'z')
 _putchar(letter);
 letter++;
 }
-
 _putchar('\n');
 }
