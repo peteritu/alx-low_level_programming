97f92df1f00431512f347612c6f218b550e588c9
peteritu <petendiritu@gmail.com>
2023-10-14 16:34:02 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-int largest_number(int a, int b, int c);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
