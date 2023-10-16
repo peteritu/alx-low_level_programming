0db178ba7a2906b7141f39e0076e8feae68343e0
peteritu <petendiritu@gmail.com>
2023-10-16 13:16:01 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string to calculate the length of.
- *
- * Return: The length of the string.
- */
+* _strlen - Returns the length of a string.
+* @s: The string to calculate the length of.
+*
+* Return: The length of the string.
+*/
 int _strlen(char *s)
 {
 int length = 0;
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0. 98 Battery st.
 1. Don't swap horses in crossing a stream
+2. This report, by its very length, defends itself against the risk of being read
