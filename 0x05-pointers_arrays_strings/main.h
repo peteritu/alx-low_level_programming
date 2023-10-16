dbcb4d49269ff0d8825f17112b720be55654df51
peteritu <petendiritu@gmail.com>
2023-10-16 16:53:02 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h> /* Include standard I/O functions */
+
 /* Function prototype for _putchar */
 int _putchar(char c);
 
-/* Function prototype for puts2 */
-void puts2(char *str);
+/* Function prototype for puts_half */
+void puts_half(char *str);
 
 #endif /* MAIN_H */
