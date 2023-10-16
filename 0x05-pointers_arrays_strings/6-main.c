c89be407f7ae5c33acc88f426d70739e02e9e0dc
peteritu <petendiritu@gmail.com>
2023-10-16 16:27:57 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-char *str;
+char *str = "Lorem ipsum..."; // Your input string
 
-str = "0123456789";
 puts2(str);
+
 return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for _putchar */
 int _putchar(char c);
-
-/* Function prototype for puts2 */
 void puts2(char *str);
 
 #endif /* MAIN_H */
