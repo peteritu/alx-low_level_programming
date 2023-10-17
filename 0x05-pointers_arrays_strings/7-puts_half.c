8e8d5fe898a79bbacd5fa540bde4a935bf9264c1
peteritu <petendiritu@gmail.com>
2023-10-16 17:02:35 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,31 +6,31 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    int start = 0;
+int length = 0;
+int start = 0;
 
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+while (str[length] != '\0')
+{
+length++;
+}
 
-    if (length % 2 == 0)
-    {
-        start = length / 2;
-    }
-    else
-    {
-        start = (length + 1) / 2;
-    }
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length + 1) / 2;
+}
 
-    while (str[start] != '\0')
-    {
-        if (str[start] != ' ' && str[start] != '\n')
-        {
-            _putchar(str[start]);
-        }
-        start++;
-    }
+while (str[start] != '\0')
+{
+if (str[start] != ' ' && str[start] != '\n')
+{
+_putchar(str[start]);
+}
+start++;
+}
 
-    _putchar('\n');
+_putchar('\n');
 }
