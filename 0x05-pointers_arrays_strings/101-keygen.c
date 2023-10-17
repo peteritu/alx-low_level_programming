feeaba6c26636eb3e2f442e61a19e94aa2f65fca
peteritu <petendiritu@gmail.com>
2023-10-16 19:37:59 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 62
+
+/**
+* main - Generates a random password
+*
+* Description:
+* This program generates a random password of a specified length.
+* The password includes lowercase letters, uppercase letters, and digits.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+srand(time(NULL)); // Initialize the random number generator with the current time
+
+char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int charset_length = PASSWORD_LENGTH;
+char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+int index = rand() % charset_length;
+password[i] = charset[index];
+}
+
+password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password
+
+printf("%s\n", password); // Print the generated password
+
+return 0;
+}
+
