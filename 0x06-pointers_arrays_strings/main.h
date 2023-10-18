1483cadb046b162ff120ee6fd526ea02945eb710
peteritu <petendiritu@gmail.com>
2023-10-18 16:50:47 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-/* Function prototype for _strncpy function */
-char *_strncpy(char *dest, char *src, int n);
+/* Function prototype for _strcmp function */
+int _strcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
 
