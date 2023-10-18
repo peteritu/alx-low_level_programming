e80a8bf231609dd7015241bf8418b58bad8cc0a3
peteritu <petendiritu@gmail.com>
2023-10-18 15:30:19 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,15 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-/* Function prototypes */
+/* Function prototype for _strcat function */
 char *_strcat(char *dest, char *src);
-char *string_toupper(char *str);
-char *cap_string(char *str);
-char *leet(char *str);
-void reverse_array(int *a, int n);
-int _strcmp(char *s1, char *s2);
+
+/* Add other function prototypes for subsequent tasks here */
 
 #endif /* MAIN_H */
 
