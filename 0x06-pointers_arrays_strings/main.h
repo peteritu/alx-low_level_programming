e72bddadabdf9b7c3e2f3343f9ba09b79324268d
peteritu <petendiritu@gmail.com>
2023-10-18 15:24:19 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Function prototypes */
+char *_strcat(char *dest, char *src);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
+
