8e7408a24bad5958e1ebdc06422da734cade3e2d
peteritu <petendiritu@gmail.com>
2023-10-22 16:11:29 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,12 +1,14 @@
+/* _putchar.c */
 #include <unistd.h>
+#include "main.h"
 
 /**
-* _putchar - writes a character to stdout
-* @c: The character to write
-*
-* Return: On success, the number of characters written is returned.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+ * _putchar - writes a character to the standard output (stdout)
+ * @c: The character to print
+ *
+ * Return: On success, the number of characters written is returned.
+ * On error, -1 is returned.
+ */
 int _putchar(char c)
 {
 return write(1, &c, 1);
