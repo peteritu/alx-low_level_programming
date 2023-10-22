41c98dcf3e834ed25ac38bbedd1843f3d57731bf
peteritu <petendiritu@gmail.com>
2023-10-22 14:50:06 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for print_diagonal function */
-void print_diagonal(int n);
+/* Function prototype for print_square function */
+void print_square(int size);
 
 /* Function prototype for _putchar function */
 int _putchar(char c);
