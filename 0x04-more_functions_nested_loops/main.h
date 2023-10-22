1d9de16314074c96362087fee97355fcc38c2828
peteritu <petendiritu@gmail.com>
2023-10-22 14:02:15 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for print_line function */
-void print_line(int n);
+/* Function prototype for print_diagonal function */
+void print_diagonal(int n);
 
 /* Function prototype for _putchar function */
 int _putchar(char c);
