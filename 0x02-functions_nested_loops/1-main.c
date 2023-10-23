7867b8b37a7eca56c785043bd037399985155998
peteritu <petendiritu@gmail.com>
2023-10-23 15:29:17 -0700

    101-crackme_password

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point to check the code.
- *
- * Return: Always 0 (Success).
- */
+* main - Entry point to check the code.
+*
+* Return: Always 0 (Success).
+*/
 int main(void)
 {
 print_alphabet();
--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,33 +1,44 @@
-#include "main.h"
+#include <stdio.h>
+#include <math.h>
 
 /**
- * main - Entry point
+ * largest_prime_factor - Find the largest prime factor of a number
+ * @n: The number to find the largest prime factor of
  *
- * Return: Always 0
+ * Return: The largest prime factor of the number
  */
+unsigned long largest_prime_factor(unsigned long n)
+{
+unsigned long largest = 0;
+
+while (n % 2 == 0)
+{
+    largest = 2;
+    n = n / 2;
+}
+
+for (unsigned long i = 3; i <= sqrt(n); i += 2)
+{
+    while (n % i == 0)
+    {
+        largest = i;
+        n = n / i;
+    }
+}
+
+if (n > 2)
+    largest = n;
+
+return largest;
+}
+
 int main(void)
 {
-long n = 612852475143;
-long divisor = 2;
-long largest_prime;
+unsigned long number = 612852475143;
+unsigned long result = largest_prime_factor(number);
 
-while (n != 0)
-{
-if (n % divisor != 0)
-divisor += 1;
-else
-{
-largest_prime = n;
-n = n / divisor;
-if (n == 1)
-{
-_putchar(largest_prime + '0');
-break;
-}
-}
-}
+printf("%lu\n", result);
 
-_putchar('\n');
 return (0);
 }
 
