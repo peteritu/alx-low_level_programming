7595ed2d8b095c28bfe6e69da0740cfeb2168ef0
peteritu <petendiritu@gmail.com>
2023-10-23 13:24:18 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,43 @@
+#include "main.h"
 #include <stdio.h>
-/**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix.
- * @a: The square matrix as a 1D array.
- * @size: The size of the square matrix.
- */
+
+void print_diagsums(int *a, int size);
+
+int main(void)
+{
+int matrix[3][3] = {
+{1, 2, 3},
+{4, 5, 6},
+{7, 8, 9}
+};
+int size = 3;
+int flat_matrix[size * size];
+int i, j, k = 0;
+
+for (i = 0; i < size; i++) {
+for (j = 0; j < size; j++) {
+flat_matrix[k] = matrix[i][j];
+k++;
+}
+}
+
+print_diagsums(flat_matrix, size);
+
+return (0);
+}
+
 void print_diagsums(int *a, int size)
 {
 int i;
 int sum_main = 0;
 int sum_secondary = 0;
-for (i = 0; i < size; i++)
-{
+
+for (i = 0; i < size; i++) {
 sum_main += a[i * size + i]; /* Main diagonal */
 sum_secondary += a[i * size + (size - 1 - i)]; /* Secondary diagonal */
 }
-putchar(sum_main / 10 + '0');
-putchar(sum_main % 10 + '0');
-putchar(',');
-putchar(' ');
-putchar(sum_secondary / 10 + '0');
-putchar(sum_secondary % 10 + '0');
-putchar('\n');
+
+printf("Sum of the main diagonal: %d\n", sum_main);
+printf("Sum of the secondary diagonal: %d\n", sum_secondary);
 }
 
