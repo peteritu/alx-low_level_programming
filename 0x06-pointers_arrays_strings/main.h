7bee3fb363aefe9817f807dbad40a12a8e54de07
peteritu <petendiritu@gmail.com>
2023-10-23 16:27:55 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for the custom _putchar function */
-int _putchar(char c);
-
-/* Function prototype for the print_number function */
-void print_number(int n);
+char *rot13(char *str);
 
 #endif /* MAIN_H */
 
