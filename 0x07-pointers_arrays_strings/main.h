b1cae387c02e6d658620484ec57b3125907c0b66
peteritu <petendiritu@gmail.com>
2023-10-23 15:10:55 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-void print_diagsums(int *a, int size);
+/* Function to set the value of a pointer to a char */
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
 
