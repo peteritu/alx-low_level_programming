f594b3b65efd15cb5cc0d22f85b65f21ad7b9ce8
peteritu <petendiritu@gmail.com>
2023-10-25 01:37:52 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c); /* Declaration for _putchar function */
+
+/* Other function prototypes or declarations, if needed */
+
+#endif /* MAIN_H */
+
