ea016a926301e776e81294e3163a4ffd1ffccf66
peteritu <petendiritu@gmail.com>
2023-10-26 15:26:02 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,23 +8,18 @@
  */
 int is_prime_number(int n)
 {
+int i;
 if (n <= 1)
-return (0); /* 0 and 1 are not prime numbers */
-
+return (0);
 if (n <= 3)
-return (1); /* 2 and 3 are prime numbers */
-
-/* Check if n is divisible by 2 or 3 */
+return (1);
 if (n % 2 == 0 || n % 3 == 0)
 return (0);
-
-/* Check for prime using 6k +/- 1 rule */
-for (int i = 5; i * i <= n; i += 6)
+for (i = 5; i * i <= n; i += 6)
 {
 if (n % i == 0 || n % (i + 2) == 0)
 return (0);
 }
-
-return (1); /* n is a prime number */
+return (1);
 }
 
