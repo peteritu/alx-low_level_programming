79aa28dc16f27c70da214ca3ec1d39bf386d8de1
peteritu <petendiritu@gmail.com>
2023-10-26 15:59:26 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,20 +5,18 @@
  * is_palindrome - Check if a string is a palindrome
  * @s: Pointer to a string
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: (1) if the string is a palindrome, (0) otherwise
  */
 int is_palindrome(char *s)
 {
-int len = 0;
-int start = 0;
-
 if (s == NULL)
 return (1);
 
+int len = 0;
 while (s[len] != '\0')
 len++;
 
-return (is_palindrome_helper(s, start, len - 1));
+return (is_palindrome_helper(s, 0, len - 1));
 }
 
 /**
@@ -27,15 +25,19 @@ return (is_palindrome_helper(s, start, len - 1));
  * @start: Starting index
  * @end: Ending index
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: (1) if the string is a palindrome, (0) otherwise
  */
 int is_palindrome_helper(char *s, int start, int end)
 {
 if (start >= end)
 return (1);
+
 if (s[start] != s[end])
 return (0);
 
-return (is_palindrome_helper(s, start + 1, end - 1));
+start++;
+end--;
+
+return (is_palindrome_helper(s, start, end));
 }
 
