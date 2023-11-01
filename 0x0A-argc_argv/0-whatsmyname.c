2cb890cdf26b8c96c20ed2fd91f19dc225c60475
peteritu <petendiritu@gmail.com>
2023-11-01 07:59:19 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
+
