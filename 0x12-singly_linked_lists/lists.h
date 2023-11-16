ee7cb7f87d5c3b6fe2684bb37c01abebae3009f9
peteritu <petendiritu@gmail.com>
2023-11-16 14:02:22 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,22 +1,26 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h> 
-/* Include this line to fix the 'unknown type name size_t' error */
+#include <stdlib.h>
 
-/* Struct definition for list node */
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *              for ALX project
+ */
 typedef struct list_s
 {
 char *str;
 unsigned int len;
 struct list_s *next;
-} list_t;
+}
+list_t;
 
-/* Function prototypes */
-size_t print_list(const list_t *h);
-/* Add prototypes for other functions here */
-
-int _putchar(char c);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
 
