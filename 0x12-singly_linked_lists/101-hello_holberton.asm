c59adfe3fa65b8bfe6a7d7207f753686cc38f018
peteritu <petendiritu@gmail.com>
2023-11-16 17:11:38 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
 section .data
-    hello db 'Hello, Holberton', 0
+hello db 'Hello, Holberton', 0
 
 section .text
-    global main
-    extern printf
+global main
+extern printf
 
 main:
-    ; Call printf
-    mov rdi, hello ; Format string address
-    mov rax, 0     ; RAX is set to 0 for printf
-    call printf
+; Call printf
+mov rdi, hello ; Format string address
+mov rax, 0     ; RAX is set to 0 for printf
+call printf
 
-    ; Exit the program
-    mov rax, 60    ; syscall: exit
-    xor rdi, rdi   ; status: 0
-    syscall
+; Exit the program
+mov rax, 60    ; syscall: exit
+xor rdi, rdi   ; status: 0
+syscall
 
