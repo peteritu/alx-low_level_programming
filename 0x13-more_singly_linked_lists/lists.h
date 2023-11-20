c89831622b14a3c8b2146c5b62803aa9ce523649
peteritu <petendiritu@gmail.com>
2023-11-20 13:16:10 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
- * @n: integer data
- * @next: points to the next node in the list
+ * @n: integer
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
@@ -17,8 +16,8 @@ int n;
 struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
-size_t print_listint(const listint_t *h);
+/* Function prototype */
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
 
